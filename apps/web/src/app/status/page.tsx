56cc3e43382ce215d40168f19544b3377c64b430
Srevin Saju <srevinsaju@sugarlabs.org>
2024-06-18 01:15:50 +0300

    fix: remove weird marketing page

--- apps/web/src/app/status/page.tsx
+++ apps/web/src/app/status/page.tsx
@@ -37,40 +37,10 @@ export const metadata: Metadata = {
 };
 
 const ExternalStatusPage = async () => {
-  const res = await fetch(env.EXTERNAL_API_URL);
-  const data = await res.json();
-  const externalStatus = externalStatusArray.parse(data);
   return (
-    <MarketingLayout>
-      <h1 className="mb-4 font-cal text-4xl text-foreground">
-        Is my external service down?
-      </h1>
-      <div className="mb-6 text-muted-foreground">
-        Easily check if your external providers is working properly
-      </div>
-      <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-2">
-        {externalStatus.map((status) => (
-          <Link
-            key={status.name}
-            href={status.url}
-            target="_blank"
-            className="group flex w-full flex-1"
-          >
-            <Card className="flex w-full flex-col">
-              <CardHeader className="flex-1">
-                <CardTitle>{status.name}</CardTitle>
-                <div className="flex flex-1 justify-between gap-3">
-                  <CardDescription className={getClassname(status)}>
-                    {status.status_description}
-                  </CardDescription>
-                  <ArrowUpRight className="h-5 w-5 shrink-0 self-end text-muted-foreground group-hover:text-foreground" />
-                </div>
-              </CardHeader>
-            </Card>
-          </Link>
-        ))}
-      </div>
-    </MarketingLayout>
+    <h1>
+    Hello World
+    </h1>
   );
 };
 
